fffa11f617b0a56528c9bf3c4a0a80cdc8baeeb0
Davide Secco <d.secco2000@gmail.com>
2021-06-08 15:44:00 +0200

    Aggiunta parte c 19Giu19 + modifica al 5GIu2015

--- EserciziMiei/2019.06.19 PROVA DA FARE ASSOLUTAMENTE/19Giu19.c
+++ EserciziMiei/2019.06.19 PROVA DA FARE ASSOLUTAMENTE/19Giu19.c
@@ -8,27 +8,42 @@
 #include <signal.h>
 #include <ctype.h>
 
+int token;
 typedef int pipe_t[2];
 
-typedef struct{
-    			/*  */
-	 			/*  */
-} s_occ;
+void scrivi(){
+	token = 1;
+}
+
+void salta(){
+	token = 0;
+}
+
+int indicemax(char vet[], int N){
+	int max=0;
+	for (int i = 1; i < N; i++){
+		if(vet[max] < vet[i]){
+			max = i;
+		}
+	}
+	return max; 
+}
 
 int main(int argc, char *argv[]){
-    int pid_nipote;				/* pid per fork */
-   	int *pid;							/* array di pid */
-
-    int H;   							/* numero di caratteri e quindi numero di processi */
+    int *pid;							/* array di pid */
+	int nr;
+	char linea[250];
+    int N;   							/* numero di caratteri e quindi numero di processi */
     int fd;      						/* per open */
     int i, j;     						/* indici, i per i figli! */
+    char c;       						/* per leggere dal file */
     pipe_t *piped;    					/* array dinamico di pipe */
     int pidFiglio, status, ritorno;    /* variabili per wait*/
-	pipe_t piped_nipote;
-	char linea[255];
-	int lunghezza;
-	int nr;
-
+	char vet[255];
+	bool b;
+	int max;
+	int nlinea;
+	int cont;
 	//CONTROLLI TIPICI
 
     /* OBBLIGATORIO: numero dei caratteri passati sulla linea di comando */
@@ -37,37 +52,40 @@ int main(int argc, char *argv[]){
         exit(1);
     }
     
-    H = argc - 1;
+    N = argc - 1;
 
-	printf("Hai inserito %d file\n", H);
-
+	printf("Sono stati inseriti %d file\n", N);
 
 	//ALLOCAZIONE MEMORIA MALLOC
 
-    /* OBBLIGATORIO: allocazione H pipe */
-    if ((piped=(pipe_t *)malloc(H*sizeof(pipe_t))) == NULL)    {
+    /* OBBLIGATORIO: allocazione N pipe */
+    if ((piped=(pipe_t *)malloc(N*sizeof(pipe_t))) == NULL)    {
     	printf("Errore allocazione pipe\n");
     	exit(3); 
     }
 
-	/* NON È DETTO SERVA: allocazione array per i pid */
-    if ((pid=(int *)malloc(H*sizeof(int))) == NULL)    {
-    	printf("Errore allocazione pid\n");
-    	exit(4);
+	/* allocazione array per i pid */
+    if ((pid = (int *)malloc(N * sizeof(int))) == NULL) {
+        printf("Errore allocazione pid\n");
+        exit(4);
     }
+
+	/* NON È DETTO SERVA:  padre aggancia le due funzioni (scrivi e salta) che useranno i figli alla ricezione dei segnali inviati dal padre */
+    bsd_signal(SIGUSR1,scrivi);
+    bsd_signal(SIGUSR2,salta);
 
 	//CREAZIONE PIPE 
 
-    /* OBBLIGATORIO: creo H pipe */
-    for (i=0; i < H; i++) {
+    /* OBBLIGATORIO: creo N pipe */
+    for (i=0; i < N; i++) {
         if (pipe(piped[i]) < 0)        {
             printf("Errore nella creazione della pipe\n");
             exit(1);
         }
 	}
 
-	printf("Sono il processo padre con pid%d e sto per generare %d figli\n", getpid(), H);
-    for (i=0; i < H; i++) {
+	printf("Sono il processo padre con pid%d e sto per generare %d figli\n", getpid(), N);
+    for (i=0; i < N; i++) {
 		/* OBBLIGATORIO: creazione dei figli */
         if ((pid[i] = fork()) < 0) {
             printf ("Errore nella fork\n");
@@ -78,9 +96,9 @@ int main(int argc, char *argv[]){
             printf("Figlio %d con pid %d\n", i, getpid());
 
 			/* OBBLIGATORIO: chiude tutte le pipe che non usa (scegli schema chiusura!) */
-			for(j = 0; j < H; j++){
-				close(piped[j][0]);		// chiusa la pipe in lettura
-				if(j != i)
+			for (j = 0; j < N; j++){
+				close(piped[j][0]);
+				if(i!=j)
 					close(piped[j][1]);
 			}
 
@@ -90,51 +108,39 @@ int main(int argc, char *argv[]){
                 exit(-1);
             }
 
-			if(pipe(piped_nipote) < 0){
-				printf("ERRORE creazione pipe nipote-figlio\n");
-				exit(5);
-			}
-
 			/* eseguo codice figlio e eventuale nipote */
-			if ((pid_nipote = fork()) < 0) {
-            	printf ("Errore nella fork del nipote \n");
-            	exit(1);
-        	}
-			
-			/* NIPOTE */
-			if(pid_nipote == 0){
-				printf("Creato il nipote %d \n", i);
-				
-				close(piped[i][1]);		// chiudo la pipe comunicazione figlio-padre
+			// leggo il file
+			j=0;
+			nlinea=0;
+			cont=0;
+			while(1){
+				nr = read(fd, &linea[j], 1);
 
-				close(1);						// chiudo lo stdinput
-				dup(piped_nipote[1]);			// duplico la pipe sullo stdin
-				close(piped_nipote[0]);
-				close(piped_nipote[1]);
+				if(linea[j] == '\n' || nr == 0){
+					nlinea++;
+					linea[j]='\0';
+					write(piped[i][1], &linea[0], 1);
+					pause();
 
-				execlp("tail", "tail ", "-1", argv[i+1], (char *)0);
+					if(token){
+						printf("Il processo %d (PID %d) carattere: %c linea: %d\n", i, getpid(), linea[0], nlinea);	
+						cont++;
+					}
 
-				perror("Problemi is wawxuIonw della tail\n");
-				exit(-1);
-			}
-			else{
-				close(piped_nipote[1]);
-				j = 0;
-				while(1){
-					nr = read(piped_nipote[0], &linea[j], 1);
-					if(linea[j] == '\n' || nr == 0){
-						linea[j]='\0';
-						lunghezza = strlen(linea);
-						write(piped[i][1], &lunghezza, sizeof(int));
+					if(nr == 0){
+						exit(cont);
 						break;
 					}
-					else 
-						j++;
+					j=0;
 				}
+				else
+					j++;
+				
+
+				
 			}
-
-
-			exit(0);
+			
+			exit(cont);
 		}
 	}
 
@@ -142,20 +148,46 @@ int main(int argc, char *argv[]){
 	printf("Padre con PID: %d\n", getpid());
     
     /* OBBLIGATORIO: chiude tutte le pipe che non usa */
-	for(i = 0; i < H; i++){
+	for(i = 0; i < N; i++){
 		close(piped[i][1]);
 	}
 
 	/* legge dalle pipe i messaggi o manda segnali?*/
-	for(i = H-1; i>=0; i--){
-		read(piped[i][0], &lunghezza, sizeof(int));
-		printf("PID: %d \t lunghezza: %d \t file: %s\n", pid[i], lunghezza, argv[i+1]);
+	j=0;
+	b=false;
+	nlinea=0;
+	while(1){
+		nlinea++;
+		
+		for(i = 0; i < N; i++){
+			nr = read(piped[i][0], &c, 1);
+			if(nr > 0){
+				b = true;
+				vet[j] = c;
+			}
+			else
+				vet[j]=0;
+			j++;
+		}
+		if(b == false){
+			break;
+		}
+		max=indicemax(vet, N);
+		printf("Linea %d\n", nlinea);
+		for(i = 0; i <N; i++){
+			if(max != i)
+				kill(pid[i], SIGUSR2);		// INVIO ai figli che non devono STAMPARE LA LINEA
+			else
+				kill(pid[i], SIGUSR1);		// INVIO IL SEGNALE ALL'UNICO FIGLIO CHE DEVE STAMPARE LA LINEA
+		}
+		sleep(1);
+		j = 0;
+		b = false;
 	}
-
 	//ATTESA TERMINAZIONE DEI FIGLI   
     /* Attesa della terminazione dei figli */
         
-    for(i=0;i < H;i++) {
+    for(i=0;i < N;i++) {
     	pidFiglio = wait(&status);
         if (pidFiglio < 0){
             printf("Errore wait\n");
